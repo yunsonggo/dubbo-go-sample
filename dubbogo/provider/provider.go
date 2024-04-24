7b7e98945df42c6ec6dd1a7d838a78f0d780c4d1
changgaojie <changgaojie@shizhuang-inc.com>
2024-04-24 16:39:25 +0800

    test dubbo

--- dubbogo/provider/provider.go
+++ dubbogo/provider/provider.go
@@ -1,13 +1,32 @@
 package provider
 
 import (
-	pcmtpb "202404/github.com/dubbo-go-sample/dubbogo/proto/gen"
+	// pcmtpb "202404/github.com/dubbo-go-sample/dubbogo/proto/gen"
 	"context"
+	"dubbo.apache.org/dubbo-go/v3/config"
 )
 
 type PcmtService struct {
 }
 
-func (p *PcmtService) Ping(ctx context.Context, req *pcmtpb.Empty) (*pcmtpb.Pong, error) {
-	return &pcmtpb.Pong{Data: "pong"}, nil
+func (p *PcmtService) Reference() string {
+    return "PcmtService"
+}
+
+// func (p *PcmtService) Ping(ctx context.Context, req *pcmtpb.Empty) (*pcmtpb.Pong, error) {
+// 	return &pcmtpb.Pong{Data: "pong"}, nil
+// }
+
+func (p *PcmtService) Ping(ctx context.Context) (string, error) {
+	return "test", nil
+}
+
+func (s *PcmtService) MethodMapper() map[string]string {
+	return map[string]string{
+		"Ping": "ping",
+	}
+}
+
+func init() {
+    config.SetProviderService(new(PcmtService))
 }
--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "202404/github.com/dubbo-go-sample/cmd"
+// import "202404/github.com/dubbo-go-sample/cmd"
+import (
+	"log"
+
+    "dubbo.apache.org/dubbo-go/v3/config"
+    _ "dubbo.apache.org/dubbo-go/v3/imports"
+    _ "202404/github.com/dubbo-go-sample/dubbogo/provider"
+)
+
 
 func main() {
-	cmd.RunDubbo()
+	// cmd.RunDubbo()
+
+	if err := config.Load(config.WithPath("/Users/admin/work/pcm/dubbo-go-sample/conf/dubbogo.yaml")); err != nil {
+		log.Fatalf("Error loading Dubbo config: %v", err)
+	}
+	
+
+	select {}
+    // quit := make(chan os.Signal, 1)
+    // signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+    // <-quit
+    // config.Stop()
 }
